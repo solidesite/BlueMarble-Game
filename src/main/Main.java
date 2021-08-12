03cfe68b08a032f59345404fa090e6c3d06944d5
solidesite <solidesite@gmail.com>
2021-08-13 04:51:19 +0900

    feat : 2인 플레이 기능 추가 + Player class

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,32 +8,46 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
-		int pos = 1;
+		ArrayList<Player> playerArr = new ArrayList<Player>();
+		playerArr.add(new Player ("플레이어1"));
+		playerArr.add(new Player ("플레이어2"));
+		Player p;
+		int playerNum = 0;
 
 		while (true) {
+			p = playerArr.get(playerNum);
 			Random rd = new Random();
 			int dice = rd.nextInt(3) + 1;
+			System.out.println(p.name + "의 차례입니다.");
+			System.out.println("현재 위치 : " + p.pos);
+			System.out.println("현재 재산 : " + p.money);
 			System.out.println("1번을 눌러 주사위를 굴리세요");
 			String input = scan.nextLine();
 			if (input.equals("1")) {
-				System.out.println("주사위" + dice);
-				pos += dice;
-				int left = pos-10;
-				if(pos > 10) {
-					pos = 0;
-					pos += left;
+				System.out.println("주사위 - <" + dice + ">");
+				p.pos += dice;
+				int left = p.pos - 10;
+				if (p.pos > 10) {
+					p.pos = 0;
+					p.pos += left;
+					p.money += 3000;
+					System.out.println("종점터치 : 2000$ 획득");
 				}
 			}
-			System.out.println("현재 위치는 : " + pos);
+			System.out.println("현재 위치 : " + p.pos);
+			System.out.println("현재 재산 : " + p.money);
 			System.out.println("무엇을 하시겠습니까?");
 			System.out.println("2.턴 넘기기");
 			String input2 = scan.nextLine();
-			if(input2.equals("2")) {
-				continue;
+			if (input2.equals("2")) {
+//				continue;
+			}
+			playerNum ++;
+			if (playerNum == playerArr.size()) {
+				playerNum = 0;
 			}
 		}
-		
+
 	}
 
 }
---
+++ src/main/Player.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Player {
+	String name;
+	int pos = 1;
+	boolean bankrupt = false;
+	int money = 10000;
+	public Player (String name) {
+		this.name = name;
+	} 
+}
