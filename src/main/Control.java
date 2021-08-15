e8f18cb7f6dbade3913f39819a8381eb78bb8f44
solidesite <solidesite@gmail.com>
2021-08-15 19:34:10 +0900

    Control class 랜덤 이벤트 기능 추가

---
+++ src/main/Control.java
@@ -0,0 +1,108 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Control {
+	Scanner scan = new Scanner(System.in);
+	Land land = new Land();
+	ArrayList<Player> playerArr = new ArrayList<Player>();
+	Player p;
+
+	public void randomEvent() {
+		System.out.println("--황금열쇠 이벤트 발생--");
+		System.out.println("1번을 눌러 주사위를 굴리세요");
+		String input = scan.nextLine();
+		Random rd = new Random();
+		int randomE = rd.nextInt(4);
+		if (input.equals("1")) {
+			if (randomE == 0) {
+				System.out.println("당신은 주차위반을 하여 벌금을 냈습니다.");
+				System.out.println("1000$를 지불했습니다.");
+				p.money -= 1000;
+			} else if (randomE == 1) {
+				System.out.println("당신은 대회에 입상하여 상금을 얻었습니다.");
+				System.out.println("1000$를 얻었습니다.");
+				p.money += 1000;
+			} else if (randomE == 2) {
+				System.out.println("당신은 로또에 당첨되었습니다.");
+				System.out.println("5000$를 얻었습니다.");
+				p.money += 5000;
+			} else if (randomE == 3) {
+//				System.out.println("당신은 파산입니다.");
+//				System.out.println("");
+//				p.money = 0;
+			}
+		}
+	}
+
+	public void start() {
+
+		Land map[] = new Land[10];
+		for (int i = 0; i < map.length; i++) {
+			map[i] = new Land();
+		}
+		map[0].event = 0;
+		map[1].event = 1;
+		map[2].event = 0;
+		map[3].event = 1;
+		map[4].event = 0;
+		map[5].event = 1;
+		map[6].event = 0;
+		map[7].event = 1;
+		map[8].event = 0;
+		map[9].event = 1;
+
+		playerArr.add(new Player("플레이어1"));
+		playerArr.add(new Player("플레이어2"));
+
+		int playerNum = 0;
+
+		while (true) {
+			p = playerArr.get(playerNum);
+			Random rd = new Random();
+			int dice = rd.nextInt(3) + 1;
+			System.out.println(p.name + "의 차례입니다.");
+			System.out.println("현재 위치 : " + p.pos);
+			System.out.println("현재 재산 : " + p.money);
+			System.out.println("1번을 눌러 주사위를 굴리세요");
+			String input = scan.nextLine();
+			if (input.equals("1")) {
+				System.out.println("주사위 - <" + dice + ">");
+				p.pos += dice;
+				int left = p.pos - 10;
+				if (p.pos > 10) {
+					p.pos = 0;
+					p.pos += left;
+					p.money += 3000;
+					System.out.println("종점터치 : 2000$ 획득");
+				}
+			}
+
+			// land 이벤트
+			if (map[p.pos].event == 1) {
+				randomEvent();
+			}
+
+			// 이동 후
+			if (p.money <= 0) {
+				System.out.println("파산했습니다. 게임 종료");
+				break;
+			}
+			System.out.println("현재 위치 : " + p.pos);
+			System.out.println("현재 재산 : " + p.money);
+			System.out.println("무엇을 하시겠습니까?");
+			System.out.println("2.턴 넘기기");
+			String input2 = scan.nextLine();
+			if (input2.equals("2")) {
+//				continue;
+			}
+			playerNum++;
+			if (playerNum == playerArr.size()) {
+				playerNum = 0;
+			}
+		}
+	}
+
+}
--- src/main/Land.java
+++ src/main/Land.java
@@ -1,6 +1,6 @@
 package main;
 
 public class Land {
-	// 이벤트 - 0없음 1랜덤이벤트 2건물매입
+	// 이벤트 - 0없음 1황금열쇠랜덤이벤트
 	int event;
 }
